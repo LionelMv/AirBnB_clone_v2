c3148a188e4b4956b3fe0ffce4ae68ee0c8dcbbf
LionelMv <mvangicode@gmail.com>
2023-07-24 22:17:48 +0300

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This scripts starts a Flask web app.
+The web app must be listening on 0.0.0.0:5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """diplays Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """diplays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_display(text):
+    """Displaying a variable"""
+    text = text.replace('_', ' ')
+    return (f'C {text}')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
