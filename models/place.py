638d28e88d1be65b264a9cdbe1f2194658b6e400
LionelMv <mvangicode@gmail.com>
2023-08-08 21:34:45 +0300

    no. 10 updated

--- models/place.py
+++ models/place.py
@@ -5,8 +5,15 @@ import models
 from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.review import Review
-from sqlalchemy import String, Column, ForeignKey, Integer, Float
+from sqlalchemy import String, Column, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -27,6 +34,9 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship('Review', backref='place',
                                cascade='all, delete-orphan')
+        amenities = relationship('Amenity',
+                                 secondary='place_amenity',
+                                 back_populates='place_amenities', viewonly=False)
 
     else:
         @property
@@ -34,3 +44,15 @@ class Place(BaseModel, Base):
             """Getter attribute in case of file storage"""
             return [review for review in models.storage.all(Review)
                     if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """Getter attribute in case of file storage"""
+            return [amenity for amenity in models.storage.all(Amenity)
+                    if amenity.id in self.amenity_ids]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter method for amenities"""
+            if (type(obj) == Amenity):
+                self.amenity_ids.append(obj.id)
